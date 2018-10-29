Test fixed, now bug in gradient

autofiber/optimization.py
@@ -162,7 +162,8 @@ def computeglobalstrain_grad(normalized_2d, fiberpoints, vertexids, stiffness_te
     dstrain_vector_du = np.divide(np.array([[dstrain_du[:, :, 0, 0]], [dstrain_du[:, :, 1, 1]], [dstrain_du[:, :, 2, 2]], [dstrain_du[:, :, 1, 2]], [dstrain_du[:, :, 0, 2]], [dstrain_du[:, :, 0, 1]]]).transpose((2, 3, 0, 1)), m).squeeze()[np.newaxis]
 
     dE_du = np.einsum('eij,ej->ei', dstrain_vector_du, dE_dstrain).reshape(dstrain_vector_du.shape[0], 3, 2)
-
+    import pdb
+    pdb.set_trace()
     point_strain_grad = np.zeros((fiberpoints.shape[0]/2, 2))
     for i in range(0, vertexids.shape[0]):
         ele_vertices = vertexids[i]

test.py
@@ -82,10 +82,16 @@ fiberpoints2d = fiberpoints2d_un.flatten()
 
 
 def f(x, *args):
-    return OP.computeglobalstrain(vertices2d_un, x, vertexids2d, stiffness_tensor3d)
+    return OP.computeglobalstrain(vertices2d_un[np.newaxis], x, vertexids2d, stiffness_tensor3d)
 
 
-res = optimize.minimize(f, fiberpoints2d, method="CG", options={'gtol': 1e-5})
+def gradf(x, *args):
+    return OP.computeglobalstrain_grad(vertices2d_un[np.newaxis], x, vertexids2d, stiffness_tensor3d)
+
+
+# grad_check = optimize.check_grad(f)
+
+res = optimize.minimize(f, fiberpoints2d, jac=gradf, method="CG", options={'gtol': 1e-8})
 
 fres = res.x.reshape(fiberpoints2d_un.shape)
 